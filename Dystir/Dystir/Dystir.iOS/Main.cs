2da1013675a1f6abfdc0c8cc92a5462c7eff1303
predrag1977 <35438980+predrag1977@users.noreply.github.com>
2022-06-06 01:43:54 +0200

    New Dystir app start

--- Dystir/Dystir/Dystir.iOS/Main.cs
+++ Dystir/Dystir/Dystir.iOS/Main.cs
@@ -1,20 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+using Dystir.iOS;
 using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
+[assembly: ExportRenderer(typeof(WebView), typeof(WebViewRenderer))]
 namespace Dystir.iOS
 {
+
     public class Application
     {
         // This is the main entry point of the application.
+        [Obsolete]
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+
+    public class WebViewRenderer : WkWebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            // Setting the background as transparent
+            this.Opaque = false;
+            this.BackgroundColor = Color.Transparent.ToUIColor();
         }
     }
 }
---
+++ Dystir/Dystir/Dystir/Views/DystirWebViewPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Dystir.Views
+{
+    public partial class DystirWebViewPage : ContentPage
+    {
+        public DystirWebViewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            DystirWebView.Source = "https://www.dystir.fo/";
+        }
+
+        [Obsolete]
+        public void OnNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            DystirWebView.Navigating += DystirWebView_Navigating;
+        }
+
+        [Obsolete]
+        private void DystirWebView_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            if (e.Url.StartsWith("file://"))
+            {
+                return;
+            }
+
+            Device.OpenUri(new Uri(e.Url));
+
+            e.Cancel = true;
+        }
+    }
+}
