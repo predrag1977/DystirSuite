cd15e90f66018ae9e30d3400a9e33599c2f4a171
predrag1977 <35438980+predrag1977@users.noreply.github.com>
2022-07-02 20:01:02 +0200

    Mobile Client changes and fix Culture Info

--- DystirWebAssembly/DystirWeb/Client/wwwroot/scripts/mobile_client_script.js
+++ DystirWebAssembly/DystirWeb/Client/wwwroot/scripts/mobile_client_script.js
@@ -1,14 +1,35 @@
 ﻿function setMainContainerMobileClientHeight() {
+    scrollButtonVisibilityMobileClient();
     var mainContainerMobileClient = document.getElementById('main_container_mobile_client');
     if (mainContainerMobileClient != null) {
-        mainContainerMobileClient.style.height = (window.innerHeight - 240) + "px";
+        mainContainerMobileClient.style.height = (window.innerHeight - 170) + "px";
     }
 }
 
 function setMatchDetailsMobileClientHeight() {
+    scrollButtonVisibilityMobileClient();
     var matchDetailsContainerMobileClient = document.getElementById('match_details_container_mobile_client');
     if (matchDetailsContainerMobileClient != null) {
         var matchDetailsStaticPart = document.getElementById('match-details_static_part');
-        matchDetailsContainerMobileClient.style.height = (window.innerHeight - 180 - matchDetailsStaticPart.offsetHeight) + "px";
+        matchDetailsContainerMobileClient.style.height = (window.innerHeight - 110 - matchDetailsStaticPart.offsetHeight) + "px";
+    }
+}
+
+function scrollButtonVisibilityMobileClient() {
+    var horizontalMenu = document.getElementById('horizontal_menu_mobile_client');
+    var horizontalMenuScroll = document.getElementById('horizontal_menu_wrapper_mobile_client');
+    if (horizontalMenu == null) {
+        return;
+    }
+    var scrollButtonLeft = document.getElementById('scroll_button_left');
+    var scrollButtonRight = document.getElementById('scroll_button_right');
+
+    if (horizontalMenu.offsetWidth >= horizontalMenuScroll.offsetWidth) {
+        scrollButtonLeft.style.visibility = "hidden";
+        scrollButtonRight.style.visibility = "hidden";
+    }
+    else {
+        scrollButtonLeft.style.visibility = "visible";
+        scrollButtonRight.style.visibility = "visible";
     }
 }
