4ff547b2314e6bdad78106fd58106b651893766f
predrag1977 <35438980+predrag1977@users.noreply.github.com>
2022-07-13 01:08:08 +0100

    Release Dystir App version 2.0.0.48 for Iphone and fixed days name

---
+++ DystirWebAssembly/DystirWeb/Client/wwwroot/scripts/match_date_format_script.js
@@ -0,0 +1,8 @@
+﻿function matchDateFormat(date) {
+    var correctDate = new Date(Date.parse(date));
+    if (isNaN(correctDate.valueOf())) {
+        return "";
+    }
+    
+    return correctDate.toLocaleDateString("fo-FO", { weekday: 'short'});
+}
