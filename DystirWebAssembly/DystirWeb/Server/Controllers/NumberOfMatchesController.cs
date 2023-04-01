23201f1ae2ae50e9ae862840085889950caa87d4
predrag1977 <35438980+predrag1977@users.noreply.github.com>
2023-04-01 02:45:30 +0200

    Full matches list for portal

--- DystirWebAssembly/DystirWeb/Server/Controllers/NumberOfMatchesController.cs
+++ DystirWebAssembly/DystirWeb/Server/Controllers/NumberOfMatchesController.cs
@@ -29,17 +29,30 @@ namespace DystirWeb.Controllers
             {
                 //return BadRequest(new UnauthorizedAccessException().Message);
 
-                var matchesCompetitionList = new List<string>();
-                foreach (var matchGroup in GetMatches())
-                {
-                    value += String.Format("{0}:{1}\n", matchGroup.Key, matchGroup.Count());
-                }
-                if (GetMatches().Count() == 0)
-                {
-                    value = "Competition:0";
-                }
+                //foreach (var matchGroup in GetMatches())
+                //{
+                //    value += String.Format("{0}:{1}\n", matchGroup.Key, matchGroup.Count());
+                //}
+                //if (GetMatches().Count() == 0)
+                //{
+                //    value = "Competition:0";
+                //}
+                var matchesCount = (GetMatchesList() ?? new List<Matches>()).Count;
+                matchesCount = matchesCount > 5 ? 5 : matchesCount;
+                value = String.Format("{0}:{1}", GetMatchesList().FirstOrDefault()?.MatchTypeName ?? "Competition", matchesCount.ToString());
             }
             return Ok(value);
+        }
+
+        private List<Matches> GetMatchesList()
+        {
+            var fromDate = DateTime.Now.Date.AddDays(0);
+            var toDate = fromDate.AddDays(0);
+            var matchesList = _dystirService.AllMatches?
+                .OrderBy(x => x.MatchTypeID).ThenBy(x => x.Time).ThenBy(x => x.MatchID)
+                .Where(x => x.Time.Value.Date >= fromDate && x.Time.Value.Date <= toDate)?.ToList();
+
+            return matchesList ?? new List<Matches>();
         }
 
         private IEnumerable<IGrouping<string, Matches>> GetMatches()
